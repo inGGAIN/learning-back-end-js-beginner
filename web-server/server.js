cf0d731e09b2f4627ec1e041f47027aa4e3a436b
inGGAIN <m.indraali105@gmail.com>
2026-02-08 19:23:22 +0800

    make it better for code of server.js

--- web-server/server.js
+++ web-server/server.js
@@ -6,25 +6,35 @@ const requestListener = (request, response) => {
     response.statusCode = 200;
 
     const { method, url } = request;
-    const name = 'Indra';
 
     if(url === '/') {
-
         if(method === 'GET') {
             return response.end('Ini adalah halaman utama');
+        } else {
+            return response.end(`Halaman tidak dapat diakses dengan ${method} request method`);
         }
-        return response.end(`Halaman tidak dapat diakses dengan ${method} request method`);
-
     } else if (url === '/about') {
+        if (method === 'GET') {
+            return response.end('Ini adalah halaman about. Silakan gunakan POST untuk mengirim data');
+        } else if(method === 'POST') {
+            let body = [];
 
-        if(method === 'POST') {
-            return response.end(`Halo, ${name} Ini adalah halaman about`);
-        } else if (method === 'GET') {
-            response.end('Ini adalah halaman about. Silakan gunakan POST untuk mengirim data');
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                return response.end(`Halo, ${name} Ini adalah halaman about`);
+            });
+        } else {
+            return response.end(`Halaman tidak dapat ditemukan dengan ${method} request method`);
         }
-        
-    }    return response.end(`Halaman tidak dapat ditemukan dengan ${method} request method`);
-}
+    } else {
+        return response.end('Halaman tidak ditemukan');
+    }
+};
 
 const server = http.createServer(requestListener);
 
