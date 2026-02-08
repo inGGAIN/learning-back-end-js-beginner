53d930fabbc8f075eabeb07bce82e5f532bc6ee3
inGGAIN <m.indraali105@gmail.com>
2026-02-08 18:48:00 +0800

    fix error while request url. to handle it, use return before response.end

--- web-server/server.js
+++ web-server/server.js
@@ -1,22 +1,36 @@
 import http from 'http';
 
-const requestListener = (request, response) => {
 
-    const method = request.method;
-    response.setHeader('Content-Type', 'text/html');
+const requestListener = (request, response) => {
+    response.setHeader("Content-Type", "text/html");
     response.statusCode = 200;
 
-    if(method == 'GET') {
-        response.end('GET')
-    }
-    if(method == 'POST') {
-        response.end('POST')
-    }
-    if(method == 'PUT') {P
+    const { method, url } = request;
+    const name = 'Indra';
+
+    if(url === '/') {
+
+        if(method === 'GET') {
+            return response.end('Ini adalah halaman utama');
+        }
+        return response.end(`Halaman tidak dapat diakses dengan ${method} request method`);
+
+    } else if (url === '/about') {
+
+        if(method === 'POST') {
+            return response.end(`Halo, ${name} Ini adalah halaman about`);
+        } else if (method === 'GET') {
+            response.end('Ini adalah halaman about. Silakan gunakan POST untuk mengirim data');
+        }
+        
+    }    return response.end(`Halaman tidak dapat ditemukan dengan ${method} request method`);
+}
+
+const server = http.createServer(requestListener);
 
 const port = 3000;
 const host = 'localhost';
 
-server.listen(port, host, ()=> {
-    console.log(`server running on http://${host}:${port}`)
-})
+    server.listen(port, host, ()=> {
+        console.log(`server running on http://${host}:${port}`);
+    });
