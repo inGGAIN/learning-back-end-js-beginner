bc4791a03687ccba4da017f283ec8760d67681e4
inGGAIN <m.indraali105@gmail.com>
2026-02-08 23:13:24 +0800

    response method, content type, text, json

--- web-server/server.js
+++ web-server/server.js
@@ -2,37 +2,46 @@ import http from 'http';
 
 
 const requestListener = (request, response) => {
-    response.setHeader("Content-Type", "text/html");
+    response.setHeader("Content-Type", "application/json");
     response.statusCode = 200;
 
     const { method, url } = request;
 
     if(url === '/') {
         if(method === 'GET') {
+            response.statusCode = 200;
             return response.end('Ini adalah halaman utama');
         } else {
+            response.statusCode = 400;
             return response.end(`Halaman tidak dapat diakses dengan ${method} request method`);
         }
     } else if (url === '/about') {
         if (method === 'GET') {
+            response.statusCode = 200;
             return response.end('Ini adalah halaman about. Silakan gunakan POST untuk mengirim data');
         } else if(method === 'POST') {
             let body = [];
-
+            
             request.on('data', (chunk) => {
                 body.push(chunk);
             });
-
+            
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
+                response.statusCode = 200;
                 return response.end(`Halo, ${name} Ini adalah halaman about`);
             });
         } else {
+            response.statusCode = 400;
             return response.end(`Halaman tidak dapat ditemukan dengan ${method} request method`);
         }
     } else {
-        return response.end('Halaman tidak ditemukan');
+        response.statusCode = 404;
+        return response.end(JSON.stringify({
+            status: 'fail',
+            message: 'Halaman tidak ditemukan'
+        }));
     }
 };
 
